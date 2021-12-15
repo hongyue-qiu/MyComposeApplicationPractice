bb766a7a03ec7fa73513d92cf79e15e3ffd56d5b
hongyue-qiu <hongyue.qiu@thoughtworks.com>
2021-12-15 16:43:07 +0800

    init

---
+++ app/src/main/java/com/example/mycomposeapplicationpractice/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.mycomposeapplicationpractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycomposeapplicationpractice.ui.theme.MyComposeApplicationPracticeTheme
+import kotlin.math.log
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeApplicationPracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(temperature: String) {
+    fun onDetailClick() {
+        // TODO go to anther page
+    }
+    Column(Modifier.fillMaxWidth().padding(16.dp)) {
+        Text(text = "Today $temperature!", modifier = Modifier.padding(0.dp,8.dp), textAlign = TextAlign.Center)
+        Button(onClick = { onDetailClick() }) {
+            Text(text = "go to detail page")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyComposeApplicationPracticeTheme {
+        Greeting("Android")
+    }
+}
