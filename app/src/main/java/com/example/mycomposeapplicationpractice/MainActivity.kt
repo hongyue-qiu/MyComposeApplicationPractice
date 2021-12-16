579017c013ac9fc5498dba3f9b47db2ee3838e14
hongyue-qiu <hongyue.qiu@thoughtworks.com>
2021-12-16 15:37:35 +0800

    abstract detail row component

--- app/src/main/java/com/example/mycomposeapplicationpractice/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapplicationpractice/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.mycomposeapplicationpractice.component.DetailRow
 
 
 class MainActivity : AppCompatActivity() {
